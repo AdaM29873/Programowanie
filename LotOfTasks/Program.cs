87bc274e19853d2a66f5f5ca3546a3472d55cadf
AdaM29873 <99401870+AdaM29873@users.noreply.github.com>
2023-01-04 14:38:48 +0100

    Add files via upload

---
+++ LotOfTasks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LotOfTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Exercise exercise = new Exercise();
+            //exercise.Exercise1();
+            //exercise.Exercise2();
+            //exercise.Exercise3();
+            //exercise.Exercise4();
+            //exercise.Exercise5();
+            //exercise.Exercise6();
+            //exercise.Exercise7();
+            //exercise.Exercise8();
+            //exercise.Exercise9();
+            Console.WriteLine("Nowy dział");
+            Console.WriteLine();
+
+            zadIf exerciseif = new zadIf();
+            //exerciseif.zadIf1();
+            //exerciseif.zadIf2();
+            //exerciseif.zadIf3();
+            //exerciseif.zadIf4();
+            exerciseif.zadIf5();
+        }
+    }
+}
